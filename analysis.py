55c0a79dfddade94c31f35ccfe51e0c28d1a8f23
eShuttleworth <shuttleworth.25@osu.edu>
2018-10-10 20:01:22 -0400

    commented out binwalk stuff for now

--- analysis.py
+++ analysis.py
@@ -1,12 +1,12 @@
-import binwalk
+# import binwalk
 
 
 def byte_stain(hex_arr):
     pass
 
 
-def binwalk_analysis(file_in):
-    bw = binwalk.scan('--signature', file_in)
-    for module in bw:
-        for result in module.results:
-            print(result.description, result.file)
+# def binwalk_analysis(file_in):
+#     bw = binwalk.scan('--signature', file_in)
+#     for module in bw:
+#         for result in module.results:
+#             print(result.description, result.file)
