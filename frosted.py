726590a16162fa18e079619167c0e93cd42ad2e7
eShuttleworth <shuttleworth.25@osu.edu>
2018-10-27 11:09:30 -0400

    added -y and -e cli args

--- frosted.py
+++ frosted.py
@@ -12,7 +12,8 @@ def parse_term():
     p.add_argument('-o', metavar='Output', type=str, required=False, help='Output file')
     p.add_argument('-s', metavar='block_size', default=64, help='Number of bytes to read in a block (default 64)', required=False)
     p.add_argument('-b', action='store_true', required=False, help='Do binwalk analysis on the file')
-    # p.add_argument('')
+    p.add_argument('-y', action='store_true', required=False, help='Byte-stain file')
+    p.add_argument('-e', action='store_true', required=False, help='Perform entropy analysis')
     return p.parse_args()
 
 
@@ -31,14 +32,24 @@ def timer(func):
 
 @timer
 def main(args):
+    f = None
+    arr = None
     # if (args.b):
     #     analysis.binwalk_analysis(args.i)
-    # else:
-    f = entropy.get_hex_file(args.i)
-    print('File read')
-    arr = entropy.file_entropy(list(f), block_size=int(args.s))
-    print('Entropy Calculated')
-    vis.export(arr, args.o, encoding='h')
+    if args.y:
+        if not f:
+            f = entropy.get_hex_file(args.i)
+            print('File read')
+        arr = analysis.byte_stain(list(f))
+        print('Bytes Stained')
+        vis.export(arr, args.o, encoding='h')
+    if args.e:
+        if not f:
+            f = entropy.get_hex_file(args.i)
+            print('File read')
+        arr = entropy.file_entropy(list(f), block_size=int(args.s))
+        print('Entropy Calculated')
+        vis.export(arr, args.o, encoding='h')
 
 
 if __name__ == '__main__':
